634584bedf69cf252e27cceb6c8b236f01ea065e
Obin Mandalika <obinnmandalikaa@gmail.com>
2024-04-17 11:22:37 +0700

    Implement unsubscribe function in Notification controller.

--- src/repository/notification.rs
+++ src/repository/notification.rs
@@ -6,7 +6,7 @@ use crate::model::notification::Notification;
 
 // Singleton of Database
 lazy_static! {
-    static ref NOTIFICATIONS: RwLock<Vec<Notification>> = RwLock::new(Vec![]);
+    static ref NOTIFICATIONS: RwLock<Vec<Notification>> = RwLock::new(vec![]);
 }
 
 pub struct NotificationRepository;
